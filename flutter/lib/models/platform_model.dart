d36123221d082f7e6c8c30a7eff4acc59211e2f6
sheedaz <52792702+sheedaz@users.noreply.github.com>
2024-07-14 22:56:39 +0800

    Update platform_model.dart

--- flutter/lib/models/platform_model.dart
+++ flutter/lib/models/platform_model.dart
@@ -5,4 +5,4 @@ import 'package:flutter_hbb/generated_bridge.dart'
 final platformFFI = PlatformFFI.instance;
 final localeName = PlatformFFI.localeName;
 
-SASHRDeskImpl get bind => platformFFI.ffiBind;
+RustDeskImpl get bind => platformFFI.ffiBind;
