3c7947437dc780a90286a5d0677c29a9e0026bfd
zees02-desk <tramaico.bangkok@gmail.com>
2024-07-19 01:35:31 +0800

    Update generate.py

--- libs/portable/generate.py
+++ libs/portable/generate.py
@@ -71,7 +71,7 @@ def build_portable(output_folder: str, target: str):
     else:
         os.system("cargo build --release")
 
-# Linux: python3 generate.py -f ../rustdesk-portable-packer/test -o . -e ./test/main.py
+# Linux: python3 generate.py -f ../sashrdesk-portable-packer/test -o . -e ./test/main.py
 # Windows: python3 .\generate.py -f ..\sashrdesk\flutter\build\windows\runner\Debug\ -o . -e ..\sashrdesk\flutter\build\windows\runner\Debug\sashrdesk.exe
 
 
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     parser.add_option("-l", "--level", dest="level", type="int",
                       help="compression level, default is 11, highest", default=11)
     (options, args) = parser.parse_args()
-    folder = options.folder or './rustdesk'
+    folder = options.folder or './sashrdesk'
     output_folder = os.path.abspath(options.output_folder or './')
 
     if not options.executable:
